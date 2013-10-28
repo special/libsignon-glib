c58707a0e145e139c1b25f86d4b6e680ec2d987d
Islam Amer <islam.amer@jollamobile.com>
2013-10-28 18:37:17 +0200

    [libsignon-glib] Patch out docs build.

--- mer-libsignon-glib/autogen.sh
+++ mer-libsignon-glib/autogen.sh
@@ -9,8 +9,30 @@ test -z "$srcdir" && srcdir=.
 	exit 1
 }
 
-cd "$srcdir"
-gtkdocize --copy --flavour no-tmpl
+# check if gtk-doc is explicitly disabled
+for ag_option in $@
+do
+   case $ag_option in
+       -disable-gtk-doc | --disable-gtk-doc)
+       enable_gtk_doc=no
+   ;;
+   esac
+done
+
+if test x$enable_gtk_doc = xno; then
+    if test -f gtk-doc.make; then :; else
+        echo "EXTRA_DIST = missing-gtk-doc" > gtk-doc.make
+    fi
+    echo "WARNING: You have disabled gtk-doc."
+    echo "         As a result, you will not be able to generate the API"
+    echo "         documentation and 'make dist' will not work."
+    echo
+else
+    echo 'Running gtkdocize'
+    cd "$srcdir"
+    gtkdocize --copy --flavour no-tmpl || exit $?
+fi
+
 cd "$OLDPWD"
 autoreconf --install --force --verbose --warnings=all "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
